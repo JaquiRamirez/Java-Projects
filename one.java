bd7d172e73669261171a256c317d5b3b1cc483a3
Jaqueline Ramirez <jramirez54@twu.edu>
2023-05-04 17:55:11 -0500

    2nd

--- one.java
+++ one.java
@@ -7,6 +7,7 @@ public class one {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
+        //--------------------------------Basic info program--------------------------
         System.out.print("Full Name? ");
         String name = keyboard.nextLine();
         
@@ -20,6 +21,43 @@ public class one {
         String phone = keyboard.nextLine();
         System.out.println("------------------------------");
         System.out.println(name + " Information" + "\n\t" + "Address: " + address +  "\n\t" + "D.O.B: " + dateOfBirth + "\n\t" + "Phone Number: " + phone);
+
+        //---------------------------Dimensions of a cuboid---------------------------------
+        System.out.println("------------------------------");
+        System.out.println("Finding the volume of a cuboid");
+        
+        //A float, incase the numbers are in decimals
+        System.out.print("Enter length: ");
+        float length = keyboard.nextFloat();
+        
+        System.out.print("Enter width: ");
+        float width = keyboard.nextFloat();
+        
+        System.out.print("Enter height: ");
+        float height = keyboard.nextFloat();
+        
+        float volume = length * width * height;
+        System.out.println("Volume of the cuboid: " + volume);
+        
+         //---------------------------Calculator---------------------------------
+         System.out.println("------------------------------");
+        //Using Float for a better accuracy on decimals
+        System.out.print("Enter a number: ");
+          
+        float num1 = keyboard.nextFloat();
+        System.out.print("Enter another number: ");
+        float num2 = keyboard.nextFloat();
+          
+        float add = num1 + num2;
+        float substraction = num1 - num2;
+        float multiplication = num1 * num2;
+        float division = num1 / num2;
+          
+        System.out.println("The two numbers add up to: " + add);
+        System.out.println("The two numbers subtract to: " + substraction);
+        System.out.println("The two numbers multiply to: " + multiplication);
+        System.out.println("The two numbers divide to: " + division);
+
 
     }
 }
